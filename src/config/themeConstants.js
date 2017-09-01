f0540404648021d380fef524dedc773c5be813b1
Adhy <adhywiranatap@gmail.com>
2017-09-01 21:20:18 +0700

    update theme constants config file

---
+++ src/config/themeConstants.js
@@ -0,0 +1,27 @@
+export const colors = {
+  red: '#E74C3C',
+  white: '#FFFFFF',
+  black: '#000000',
+};
+
+export const flexPos = {
+  center: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerHorizontal: {
+    justifyContent: 'center',
+  },
+  centerVertical: {
+    alignItems: 'center',
+  }
+};
+
+export const fontWeightScale = {
+  normal: {
+    fontWeight: 'normal',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+};
