04cf5d5b47647a0e56ee1a504dcae238ccf6207a
Adhy <adhywiranatap@gmail.com>
2017-09-02 20:49:44 +0700

    remove redux-form integration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
 
 import commandReducer from './commandReducer';
 import chatReducer from './chatReducer';
@@ -8,5 +7,4 @@ export default combineReducers({
   template: () => ({ data: 'wow' }),
   command: commandReducer,
   chat: chatReducer,
-  form: formReducer,
 });
