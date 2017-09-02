9c582b48c6c2c092f5323cee21921297221e6e1b
Adhy <adhywiranatap@gmail.com>
2017-09-02 18:00:35 +0700

    update chatReducer state mutations

--- src/reducers/chatReducer.js
+++ src/reducers/chatReducer.js
@@ -1,5 +1,11 @@
 import Immutable from 'seamless-immutable';
 
+import {
+  FETCH_CHATS_LOADING,
+  FETCH_CHATS_SUCCESS,
+  FETCH_CHATS_FAILURE,
+} from '../actions/constants';
+
 const initialState = Immutable({
   data: [
     {
@@ -19,10 +25,25 @@ const initialState = Immutable({
   ],
   isFetching: false,
   isError: false,
+  error: null,
 });
 
+const fetchChatSuccess = (state, data) => {
+  let modifiedState = Immutable.set(state, 'isFetching', false);
+  modifiedState = Immutable.set(modifiedState, 'error', null);
+  return Immutable.set(modifiedState, 'data', data);
+};
+
+const fetchChatFailure = (state, error) => {
+  const modifiedState = Immutable.set(state, 'isFetching', false);
+  return Immutable.set(modifiedState, 'error', error);
+};
+
 export default (state = initialState, { type, payload }) => {
-  switch(type) {
+  switch (type) {
+    case FETCH_CHATS_LOADING: return Immutable.set(state, 'isFetching', true);
+    case FETCH_CHATS_SUCCESS: return fetchChatSuccess(state, payload);
+    case FETCH_CHATS_FAILURE: return fetchChatFailure(state, payload);
     default: return state;
   }
 };
