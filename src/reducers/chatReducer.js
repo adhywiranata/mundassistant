a570a9352c6d91ea1927ce8d4aebc46112de9fd0
Adhy <adhywiranatap@gmail.com>
2017-09-02 14:24:46 +0700

    modify initial chat state

--- src/reducers/chatReducer.js
+++ src/reducers/chatReducer.js
@@ -5,14 +5,14 @@ const initialState = Immutable({
     {
       id: 1,
       bot: true,
-      message: 'Hello! Anything i can help with?',
+      message: 'Hello! Anything i can help with? bla bla bla jibberish jibber jabber',
       read: true,
       createdAt: new Date(),
     },
     {
       id: 2,
       bot: false,
-      message: 'Can you help me with this.. ?',
+      message: 'Can you help me with this.. ? bla bla bla jibberish jibber jabber',
       read: true,
       createdAt: new Date(),
     },
