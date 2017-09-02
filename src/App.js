3155e977e6f009fe4d99a3b623fc405e101c8c84
Adhy <adhywiranatap@gmail.com>
2017-09-02 13:36:54 +0700

    add initial redux reducers and store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './store/configureStore';
 
 import SplashScreen from './screens/Splash';
 import HomeScreen from './screens/Home';
 import ChatScreen from './screens/Chat';
 
-export default StackNavigator({
+const MainNavigator = StackNavigator({
   Home: { screen: HomeScreen },
   Splash: { screen: SplashScreen },
   Chat: { screen: ChatScreen },
 });
+
+export default () => (
+  <Provider store={store}>
+    <MainNavigator />
+  </Provider>
+);
